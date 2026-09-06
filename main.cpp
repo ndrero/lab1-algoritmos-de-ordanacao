47f3a79687fd00402532225cd4910b4708df456b
Andrei Carvalho <andreicarvs@gmail.com>
2026-09-06 01:28:26 -0300

    feat: adiciona a criação de vetor com opções de geração de numero e exibição de dados do vetor

--- main.cpp
+++ main.cpp
@@ -4,9 +4,40 @@
 
 using namespace std;
 
+void showNumbers(int *A, int n){
+    for(int i = 0; i < n; i++){
+        cout << A[i] << endl;
+    }
+}
+
+void generateRandomNumbers(int *Array, int n){;
+    for(int i = 0; i < n; i++)
+        Array[i] = rand() % 100; // % 100 para pegar apenas numeros abaixo de 100 para melhor visualização
+};
+
+void generateOrderedNumbers(int *Array, int n){
+    for(int i = 0; i < n; i++)
+        Array[i] = i + 1;
+};
+
+void generateInverseOrderedNumbers(int *Array, int n){
+    for(int i = 0; i < n; i++)
+        Array[i] = n - i;
+};
+
+void generateMixedOrder(int *Array, int n){
+    for(int i = 0; i < n/2; i++)
+        Array[i] = i + 1;
+    for(int i = n/2; i < n; i++)
+        Array[i] = rand() % 100;
+};
+
+
 int main(){
     int option;
     int n = 0;
+    int dataGenerationType;
+    int* mainArray = nullptr;
 
     do {
         cout << "\n=== MENU PRINCIPAL ===" << endl;
@@ -25,9 +56,41 @@ int main(){
         switch (option)
         {
         case 1:
+            cout << "Informe o tamanho do array: ";
+            cin >> n;
+            mainArray = new int[n];
+
+            cout << "Informe o tipo de geração de dados: " << endl;
+            cout << "[1] Dados aleatórios" << endl ;
+            cout << "[2] Dados previamente ordenados" << endl ;
+            cout << "[3] Dados em ordem inversa" << endl ;
+            cout << "[4] Dados parcialmente ordenados" << endl ;
+            cin >> dataGenerationType;
+            switch (dataGenerationType)
+            {
+            case 1:
+                generateRandomNumbers(mainArray, n);
+                break;
+            case 2:
+                generateRandomNumbers(mainArray, n);
+                break;
+            case 3:
+                generateInverseOrderedNumbers(mainArray, n);
+                break;
+            case 4:
+                generateMixedOrder(mainArray, n);
+                break;
+            
+            default:
+                break;
+            }
             break;
         case 2:
-            /* code */
+            if (mainArray == nullptr) {
+                cout << "Nenhum vetor definido";
+                break;
+            } 
+            showNumbers(mainArray, n);
             break;
         case 3:
             /* code */
